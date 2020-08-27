12720807142fd7cbf7f769bc0b50013a2ff4c415
Raywando <33800255+Raywando@users.noreply.github.com>
2020-08-27 08:25:57 +0300

    Update vaxser.sh

--- vaxser.sh
+++ vaxser.sh
@@ -3,19 +3,15 @@ echo "====================== It's gonna be Legen... Wait for it ================
 cat tmp | concurl -c 100 -d 10 &>/dev/null
 
 cd out
-out=""
 
 for source in $(find -type f)
 do
         url=$(cat $source | head -1 | awk '{print $4}')
         for var in $(cat $source | grep -oP "var [a-z0-9_]+" | awk '{print $2}')
         do
-                if ! [[ $(echo $out) == *"$url?$var=\";alert(69)//"* ]]
-                then
-                        echo "$url?$var=\";alert(69)//"
-                        out="$out,$url?$var=\";alert(69)//"
-                fi
+                echo "$url?$var=\";alert(69)//" >> temp
         done
 done
+cat temp | sort -u
 cd .. && rm tmp && rm out -r
 echo "======================================= ..DARY! ================================"
